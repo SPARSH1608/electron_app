e68396068300793de0a2eadd18d012de3a9ccf14
SPARSH1608 <sparshgoelk@gmail.com>
2025-03-13 13:13:09 +0530

    AppImage working fine

--- main.js
+++ main.js
@@ -1,32 +1,87 @@
-const { app, BrowserWindow, ipcMain } = require("electron");
+const { app, BrowserWindow, globalShortcut, ipcMain } = require('electron');
+const path = require('path');
 
+// Add this near the top of your file
 let mainWindow;
 
-app.whenReady().then(() => {
+// Add this before createWindow function
+ipcMain.on('close-app', () => {
+  app.quit();
+});
+
+function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    fullscreen: true, 
-    kiosk: true, 
-    resizable: false, 
-    minimizable: false, 
+    fullscreen: true,
+    frame: false,
+    resizable: false,
+    minimizable: false,
     maximizable: false,
-    frame: false, 
     alwaysOnTop: true,
+    kiosk: true,
     webPreferences: {
-      nodeIntegration: true, 
-      contextIsolation: false,
-    },
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js')
+    }
   });
 
-  mainWindow.loadFile("index.html");
+  // Load the index.html file
+  mainWindow.loadFile('index.html');
 
-
-  ipcMain.on("close-app", () => {
-    app.quit();
+  // Block navigation to external sites
+  mainWindow.webContents.on('will-navigate', (event, url) => {
+    const currentURL = mainWindow.webContents.getURL();
+    const baseURL = 'file://' + __dirname;
+    
+    if (!url.startsWith(baseURL)) {
+      event.preventDefault();
+    }
   });
 
-  app.on("window-all-closed", () => {
-    app.quit();
+  // Block new window creation
+  mainWindow.webContents.setWindowOpenHandler(() => {
+    return { action: 'deny' };
   });
+
+  // Disable Alt+Tab, Win key, Alt+F4, etc.
+  mainWindow.webContents.on('before-input-event', (event, input) => {
+    // Block Alt+Tab, Windows key and other system keys
+    if (input.key === 'Tab' && input.alt ||
+        input.key === 'F4' && input.alt ||
+        input.key === 'Meta') {
+      event.preventDefault();
+    }
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  // Register global shortcuts to prevent Alt+Tab, etc.
+  globalShortcut.register('Alt+Tab', () => {
+    return false;
+  });
+  
+  globalShortcut.register('Super', () => {
+    return false;
+  });
+  
+  globalShortcut.register('CommandOrControl+Tab', () => {
+    return false;
+  });
+  
+  globalShortcut.register('Alt+F4', () => {
+    return false;
+  });
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  globalShortcut.unregisterAll();
+  if (process.platform !== 'darwin') app.quit();
 });
